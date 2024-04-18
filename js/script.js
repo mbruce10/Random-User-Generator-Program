73ec6203fca4448914a30d890f35c85d0440c1a9
mbruce10 <megleigh2462@gmail.com>
2024-04-18 07:59:15 -0400

    Added a dropdown menu

--- js/script.js
+++ js/script.js
@@ -1,8 +1,9 @@
 const randomFolks = document.querySelector(".random-peeps");
+const selectUserNumber = document.querySelector("#users");
 
-const getData = async function () {
+const getData = async function (numUsers) {
     const usersRequest = await fetch(
-        "https://randomuser.me/api?results=5"
+        `https://randomuser.me/api?results=${numUsers}`
     );
     const data = await usersRequest.json();
     // console.log(data);
@@ -12,7 +13,7 @@ const getData = async function () {
     displayUsers(userResults);
 };
 
-getData();
+getData(1);
 
 const displayUsers = function (userResults) {
     randomFolks.innerHTML = "";
@@ -33,4 +34,9 @@ const displayUsers = function (userResults) {
 
     }
 };
+
+selectUserNumber.addEventListener("change", function (e) {
+    const numUsers = e.target.value;
+    getData(numUsers);
+});
 
